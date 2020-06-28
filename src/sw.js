73a33b787e8439d998fb51132997b7eae590940b
Steven Zelek <szelek@chathamfinancial.com>
2020-06-28 15:29:03 -0400

    sw hunt #5

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,8 @@
 
+self.addEventListener('install', function () {
+    self.skipWaiting();
+});
+
 export function unregister() {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.ready.then(function (registration) {
