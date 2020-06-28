fab48ede61c9fc331fd81b47b65fed13ac6f64b6
Steven Zelek <szelek@chathamfinancial.com>
2020-06-28 15:00:03 -0400

    try service worker unregister for all visitors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
 if (navigator.serviceWorker) {
     navigator.serviceWorker.getRegistrations().then(function (registrations) {
         for (let registration of registrations) {
@@ -17,3 +10,10 @@ if (navigator.serviceWorker) {
         }
     })
 }
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
